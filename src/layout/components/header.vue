ddfcc762660748abb1175cd21a483773c00a7d77
suxrobnayimov <suxrobnayimov002@gmail.com>
2022-10-13 13:57:22 +0500

    added header section

--- src/layout/components/header.vue
+++ src/layout/components/header.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="app">
-		<!-- <div class="container">
-			<nav class="">
-				<div class="navbar-items">
-					<router-link to="/">
-						<img src="@/assets/images/img/admin-logo.svg" alt="" />
-					</router-link>
-				</div>
-				<div class="navbar-items">
-					
-				</div>
-			</nav>
-		</div> -->
-		<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 items-center">
+		<nav class="bg-white border-gray-200 pt-1 pb-1 rounded dark:bg-gray-900 items-center">
 			<div class="container flex flex-wrap justify-between items-center mx-auto">
 				<a href="/home" class="flex items-center">
 					<img src="@/assets/images/img/admin-logo.svg" alt="" />
@@ -25,7 +13,7 @@
 					<ul class="flex flex-col items-center p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 						<li>
 							<!-- <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page"> -->
-								<button type="button" class="user-btn text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex justify-between items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 mr-2 mb-2">
+								<button type="button" class="user-btn text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex justify-between items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600">
 									Shohrux
 									<div class="user-icon">
 										<img class="image" src="@/assets/images/icons/icon-user.svg" alt="">
@@ -34,7 +22,7 @@
 							<!-- </a> -->
 						</li>
 						<li>
-							<a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+							<a href="/auth/login" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
 								<img src="@/assets/images/icons/log-out.svg" alt="" />
 							</a>
 						</li>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem praesentium soluta aut id dolores eaque corporis, odio fugit qui quo doloribus doloremque quam dolorem labore voluptas magnam quasi minima? Enim.</h2>
+  <div class="app">
+    
   </div>
 </template>
 
